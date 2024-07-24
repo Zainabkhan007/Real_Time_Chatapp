2ab667d606baeda659f6bbf886f127329ee33ab1
Zainabkhan007 <zk8s4daf@gmail.com>
2024-07-24 13:41:01 -0700

    second commit

--- real_time_chatapp/real_time_chatapp/settings.py
+++ real_time_chatapp/real_time_chatapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-$!(z0fy)6hkx@q9q7u!zvu%3k+3tw=ogfdoptv%3ic&=j!x+*5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
-# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
